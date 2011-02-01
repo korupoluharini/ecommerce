0bdc221df03c17847744daf97188839262e51add
korupoluharini <harini.rails@gmail.com>
2011-02-01 16:23:46 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ecommerce::Application.config.secret_token = 'c3b5e548ee7e4311b68a05659a0bba4f76c5a2052128c35efd75d8853e20885eab472534bdf5db6df266b82eded57809d9d255a24be19880bfcdbba1d3605c11'
